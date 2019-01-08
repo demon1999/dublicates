43c988ccf8202d08c4a86dd6a87a5c64eca2015c
demon1999 <drsanusha1@mail.ru>
2019-01-08 20:49:36 +0300

    getting equal files

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMap>
+#include <QVector>
+#include <QString>
+#include <QFile>
+#include <QTextStream>
+#include <QIODevice>
+#include <QCryptographicHash>
 #include <QCommonStyle>
 #include <QDesktopWidget>
 #include <QDir>
@@ -26,8 +33,8 @@ main_window::main_window(QWidget *parent)
     connect(ui->actionScan_Directory, &QAction::triggered, this, &main_window::select_directory);
     connect(ui->actionExit, &QAction::triggered, this, &QWidget::close);
     connect(ui->actionAbout, &QAction::triggered, this, &main_window::show_about_dialog);
-
-    scan_directory(QDir::currentPath(), true);
+    QMap<QString, QVector<QString>> data;
+    scan_directory(QDir::currentPath(), true, data);
 }
 
 main_window::~main_window()
@@ -37,36 +44,56 @@ void main_window::select_directory()
 {
     QString dir = QFileDialog::getExistingDirectory(this, "Select Directory for Scanning",
                                                     QString(), QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
-
-    scan_directory(dir, true);
+    QMap<QString, QVector<QString>> data;
+    scan_directory(dir, true, data);
 }
 
-void main_window::scan_directory(QString const& dir, bool is_first)
+void main_window::scan_directory(QString const& dir, bool is_first, QMap<QString, QVector<QString> >& data)
 {
     if (is_first) {
         ui->treeWidget->clear();
         setWindowTitle(QString("Directory Content - %1").arg(dir));
     }
     QDir d(dir);
-    /*if (!is_first) {
-        QTreeWidgetItem* item = new QTreeWidgetItem(ui->treeWidget);
-        item->setText(0, d.absolutePath());
-        item->setText(1, QString::number(1));
-        ui->treeWidget->addTopLevelItem(item);
-        return;
-    }*/
 
     QFileInfoList list = d.entryInfoList(QDir::AllEntries | QDir::NoDotAndDotDot);
 
     for (QFileInfo file_info : list)
     {
         if (file_info.isDir()) {
-            scan_directory(file_info.absoluteFilePath(), false);
+            //continue;
+            scan_directory(file_info.absoluteFilePath(), false, data);
         } else {
-            QTreeWidgetItem* item = new QTreeWidgetItem(ui->treeWidget);
-            item->setText(0, file_info.absoluteFilePath());
-            item->setText(1, QString::number(file_info.size()));
-            ui->treeWidget->addTopLevelItem(item);
+            QCryptographicHash hs(QCryptographicHash::Algorithm::Sha256);
+            //hs.addData(file_info.absoluteFilePath().toStdString().c_str(), file_info.absoluteFilePath().size());
+            QFile file(file_info.absoluteFilePath());
+            if(!file.open(QIODevice::ReadOnly)) {
+                //file.setErrorString(QString("You're Fucking mothers son!"));
+                QMessageBox::information(0, "error", "You're fucking mother's son!");
+            }
+
+            QTextStream in(&file);
+
+            while(!in.atEnd()) {
+                QString line = in.readLine();
+                hs.addData(line.toStdString().c_str());
+            }
+
+            file.close();
+            data[QString(hs.result().toHex())].append(file_info.absoluteFilePath());
+
+        }
+    }
+    if (is_first == true) {
+        int number_of_types = 0;
+        for (auto v : data) {
+            for (auto eq_files : v) {
+                QTreeWidgetItem* item = new QTreeWidgetItem(ui->treeWidget);
+                item->setText(0, eq_files);
+                item->setText(1, "Type: " + QString::number(QFile(eq_files).size()));
+                ui->treeWidget->addTopLevelItem(item);
+            }
+            number_of_types++;
         }
     }
 }
